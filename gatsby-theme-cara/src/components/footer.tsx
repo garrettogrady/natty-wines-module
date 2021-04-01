efa55a4a157bfc8f504642d8e03da2455c81ce67
Garrett O'Grady <Garrettogrady@gmail.com>
2021-03-31 20:02:14 -0700

    first commit

---
+++ gatsby-theme-cara/src/components/footer.tsx
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+      <br />
+    </Box>
+  )
+}
+
+export default Footer
